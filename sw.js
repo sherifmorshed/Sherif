b07660f115050ff51451b777a6a1db0485fd949c
sherifmorshed <74205549+sherifmorshed@users.noreply.github.com>
2026-03-27 12:49:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,25 @@
-const CACHE_NAME = 'wells-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'sherif-wells-v1';
+const ASSETS = [
+  '/Sherif/',
+  '/Sherif/index.html',
+  '/Sherif/manifest.json',
+  '/Sherif/icon.png'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+// Fetch Assets from Cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
